bf7e2e2740da67c3f3f33623d2e74cfcea4c4543
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-28 20:31:31 +0000

    resources added

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -5,8 +5,8 @@ provider "aws" {
 }
 
 
-resource "aws_iam_role" "example" {
-  name = "example-role"
+resource "aws_iam_role" "sys_74" {
+  name = "sys-74-role"
 
   assume_role_policy = <<EOF
 {
@@ -27,7 +27,7 @@ EOF
 
 resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-  role       = aws_iam_role.example.name
+  role       = aws_iam_role.sys_74.name
 }
 
 
@@ -56,3 +56,25 @@ resource "aws_iam_role" "role" {
 }
 EOF
 }
+
+resource "aws_sns_topic" "sys_74" {
+  name = "sys_74-topic"
+}
+
+resource "aws_codedeploy_app" "sys_74" {
+  compute_platform = "ECS"
+  name             = "sys-74_codedeploy_app"
+  # service_role_arn       = aws_iam_role.sys_74.name
+}
+
+resource "aws_codedeploy_deployment_group" "sys_74" {
+  app_name              = aws_codedeploy_app.sys_74.name
+  deployment_group_name = "sys_74-group"
+  service_role_arn      = aws_iam_role.sys_74.arn
+
+trigger_configuration {
+    trigger_events     = ["DeploymentFailure"]
+    trigger_name       = "sys_74-trigger"
+    trigger_target_arn = aws_sns_topic.sys_74.arn
+  }
+} 
